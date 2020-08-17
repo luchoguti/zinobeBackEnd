8d3d6ac1ed4a31ebf69d1fceeb0607270e9c6630
luis.torres <luchoguti9@gmail.com>
2020-08-17 11:58:27 -0500

    first commit

---
+++ bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+require "vendor/autoload.php";
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+
+    "driver" => "mysql",
+
+    "host" =>"127.0.0.1",
+
+    "database" => "zinobe_back_end",
+
+    "username" => "root",
+
+    "password" => ""
+
+]);
+
+$capsule->setAsGlobal();
+
+$capsule->bootEloquent();
