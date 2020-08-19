7b5f73843d4d183e33fef1b749e60609e5364586
luis.torres <luchoguti9@gmail.com>
2020-08-19 02:36:37 -0500

    pre-production changes of configurations are made

--- cache/78217d65a6ea3d45c281883e4377b7823a019738.php
+++ cache/78217d65a6ea3d45c281883e4377b7823a019738.php
@@ -4,7 +4,7 @@
 
 <?php $__env->startSection('sidebar'); ?>
     <nav class="navbar navbar-light bg-light">
-        <a class="navbar-brand" href="/zinobeBackEnd/User/create_user_view/">Create User</a>
+        <a class="navbar-brand" href="https://shielded-fjord-53139.herokuapp.com/User/create_user_view/">Create User</a>
     </nav>
 <?php $__env->stopSection(); ?>
 
@@ -23,7 +23,7 @@
             </div>
         </div>
     <?php endif; ?>
-    <form class="pt-4" method="post" action="/zinobeBackEnd/Login/login">
+    <form class="pt-4" method="post" action="https://shielded-fjord-53139.herokuapp.com/Login/login">
         <div class="form-group ">
             <label for="exampleInputEmail1">Email address</label>
             <?php if(isset($dataForm['email_login'])): ?>
